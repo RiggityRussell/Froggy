a0176f9ac67c5b1e24d26f71bf39b8657e7a9a9d
RiggityRussell <arlt9@mail.nmc.edu>
2022-07-18 21:20:10 -0400

    main update, settings created

---
+++ code/main.py
@@ -0,0 +1,24 @@
+import pygame, sys #importing pygame to run our game, and system commands
+from settings import * #importing our settings we create for the game
+
+class Game: #the game class which holds the main information to run the game.
+    def __init__(self): #defining the game class
+
+        pygame.init() #initializing pygame
+        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        self.clock = pygame.time.Clock()
+    
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
